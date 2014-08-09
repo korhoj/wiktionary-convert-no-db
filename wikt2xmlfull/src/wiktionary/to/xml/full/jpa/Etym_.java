65436f855f185e628404a0655a1b23cc78f3147b
korhonenjoel <korhonenjoel@gmail.com>
2014-08-09 22:40:29 +0000

    Built now with Eclipse Luna, Java 8 and Hibernate 4.3.6

---
+++ wikt2xmlfull/src/wiktionary/to/xml/full/jpa/Etym_.java
@@ -0,0 +1,13 @@
+package wiktionary.to.xml.full.jpa;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2013-10-29T08:22:15.562+0200")
+@StaticMetamodel(Etym.class)
+public class Etym_ {
+	public static volatile SingularAttribute<Etym, Integer> id;
+	public static volatile SingularAttribute<Etym, WordEtym> wordEtym;
+	public static volatile SingularAttribute<Etym, String> dataField;
+}
