b63c33cdb3b727c083cb3130b21fc7246854b976
korhoj <korhoj@outlook.com>
2016-11-07 11:02:02 +0200

    Windows script for removing extraneous namespaces from the Albanian Wiktionary

---
+++ wikt2xmlfull/StripNamespaces sqwiktionary.cmd
@@ -0,0 +1,39 @@
+@echo off
+rem Process Albanian Wiktionary
+rem Joel Korhonen 2016-Nov-07
+rem set JAVA_HOME=C:\PROGRA~1\Java\jdk1.8.0_102
+rem
+rem Before running, set JAVA_HOME=C:\PROGRA~1\Java\jdk1.8.0_xxx in your environment settings
+rem Set WIKT to location of the runnable JAR file
+rem Set DICT to location under which you uncompressed the Wiktionary edition you have downloaded
+rem e.g. DICT=C:\Temp\enwiktionary\
+rem   having uncompressed enwiktionary-%EDITION%-pages-articles.xml.zip to
+rem   C:\Temp\enwiktionaryenwiktionary-%EDITION%-pages-articles.xml
+rem Then change EDITION to match with the Wiktionary edition you have downloaded
+set EDITION=20161101
+set LANG=sq
+
+SET PROGDIR=%WIKT%
+SET PROG=%PROGDIR%\StripNamespaces.jar
+SET JCLASS=wiktionary\to\xml\full\StripNamespaces
+SET JAVA="%JAVA_HOME%\bin\java.exe"
+SET INFILE=%DICT%\%LANG%wiktionary-%EDITION%-pages-articles.xml\%LANG%wiktionary-%EDITION%-pages-articles.xml
+SET OUTFILE=%DICT%\%LANG%wiktionary-%EDITION%-pages-articles.xml\stripped-ALL.xml
+SET UTF8=-Dfile.encoding=UTF8
+
+cd %PROGDIR%
+chcp 65001
+
+%JAVA% %UTF8% -jar %PROG% %INFILE% %OUTFILE%
+if %ERRORLEVEL% GTR 0 goto :virhe
+echo Ready
+
+if "%1" == "" pause
+goto end
+
+:virhe
+echo Ended in error
+pause
+exit 8
+
+:end
