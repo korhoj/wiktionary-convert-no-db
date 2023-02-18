3c05ad10b6f1c320e49d538fbc0608557d5a334e
Joel Korhonen <korhoj@outlook.com>
2023-02-18 21:22:37 +0200

    Scripts and language codes for Norwegian
    Includes "Norwegian" (no), "Bokmål" (nb) and "Nynorsk" (nn)

---
+++ wikt2xmlfull/join-dupls-dictfmt-nn-ALL.cmd
@@ -0,0 +1,16 @@
+@echo off
+rem Joel Korhonen 2023-02-18
+set convLangCode="nn"
+set convLang="ALL"
+set convDate="2023-02-18"
+set JAVA_HOME=C:\Usr\jdk-19
+SET JAVA="%JAVA_HOME%\bin\java.exe"
+SET JAR_DIR=G:\Dropbox\Dictionary\wikt
+SET WIKT=C:\Users\korho\git\wiktionary-convert-no-db\wikt2xmlfull
+SET PROG=%JAR_DIR%\JoinDefinitions.jar
+SET JCLASS=wiktionary\to\xml\full\JoinDefinitions
+SET UTF8=-Dfile.encoding=UTF8
+SET INFILE=F:\Users\Joel\OwnStarDict-ready\wikt-%convLangCode%-%convLang%-%convDate%.txt
+SET OUTFILE=F:\Users\Joel\OwnStarDict-ready\wikt-%convLangCode%-%convLang%-dictfmt-dupls-joined.txt
+%JAVA% %UTF8% -jar %PROG% %INFILE% %OUTFILE%
+pause
