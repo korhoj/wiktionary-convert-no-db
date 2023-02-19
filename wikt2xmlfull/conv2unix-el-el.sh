275f76c8704eb604f3657d86b616990eab458510
Joel Korhonen <korhoj@outlook.com>
2023-02-19 12:34:13 +0200

    conv2unix scripts use the current date automatically now

--- wikt2xmlfull/conv2unix-el-el.sh
+++ wikt2xmlfull/conv2unix-el-el.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 convLangCode="el"
 convLang="el"
-convDate="2023-02-18*"
-FILES="wikt-$convLangCode-$convLang-$convDate.txt"
+#convDate="2023-02-19*"
+#FILES="wikt-$convLangCode-$convLang-$convDate.txt"
 # Find out YYYY-MM-DD automatically
-#FILES="wikt-$convLangCode-$convLang-`date %Y-%m-%d*.txt`"
+FILES="wikt-$convLangCode-$convLang-`date +%Y-%m-%d*.txt`"
+[ -f toiconv-wikt-$convLangCode-$convLang.txt ] && rm -v toiconv-wikt-$convLangCode-$convLang.txt
 [ -f wikt-$convLangCode-$convLang-unsorted.txt ] && rm -v wikt-$convLangCode-$convLang-unsorted.txt
 for f in $FILES
 do
   echo Processing file: ${f}
   dos2unix ${f}
-  cat ${f} >> wikt-$convLangCode-$convLang-unsorted.txt
+  cat ${f} >> toiconv-wikt-$convLangCode-$convLang.txt
 done
+[ -f utf16.tmp ] && rm utf16.tmp
+iconv -c -f utf-8 -t utf-16 toiconv-wikt-$convLangCode-$convLang.txt > utf16.tmp
+iconv -c -f utf-16 -t utf-8 utf16.tmp > wikt-$convLangCode-$convLang-unsorted.txt
+[ -f utf16.tmp ] && rm utf16.tmp
+#iconv -c -f utf-16 -t utf-8 toiconv-wikt-$convLangCode-$convLang.txt > wikt-$convLangCode-$convLang-unsorted.txt
--- wikt2xmlfull/conv2unix-en-ALL.sh
+++ wikt2xmlfull/conv2unix-en-ALL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 convLangCode="en"
 convLang="ALL"
-convDate="2023-02-18*"
-FILES="wikt-$convLangCode-$convLang-$convDate.txt"
+#convDate="2023-02-19*"
+#FILES="wikt-$convLangCode-$convLang-$convDate.txt"
 # Find out YYYY-MM-DD automatically
-#FILES="wikt-$convLangCode-$convLang-`date %Y-%m-%d*.txt`"
+FILES="wikt-$convLangCode-$convLang-`date +%Y-%m-%d*.txt`"
 [ -f wikt-$convLangCode-$convLang-unsorted.txt ] && rm -v wikt-$convLangCode-$convLang-unsorted.txt
 for f in $FILES
 do
